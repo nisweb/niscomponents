e806a5547feab18329e10c622daf6ddd524f3653
nisdesigner1 <nis.designer1@gmail.com>
2026-04-13 15:16:49 +0530

    nis traker html
    one page html

---
+++ nistraker/html/js/main.js
@@ -0,0 +1,59 @@
+(function ($) {
+    "use strict";
+
+  
+    // Initiate the wowjs
+    new WOW().init();
+    
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 45) {
+            $('.nav-bar').addClass('sticky-top shadow-sm').css('top', '0px');
+        } else {
+            $('.nav-bar').removeClass('sticky-top shadow-sm').css('top', '-100px');
+        }
+    });
+
+
+    // Header carousel
+    $(".header-carousel").owlCarousel({
+        animateOut: 'fadeOut',
+        items: 1,
+        margin: 0,
+        stagePadding: 0,
+        autoplay: true,
+        smartSpeed: 500,
+        dots: true,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+    });
+
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 5,
+        time: 2000
+    });
+
+
+   // Back to top button
+   $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+        $('.back-to-top').fadeIn('slow');
+    } else {
+        $('.back-to-top').fadeOut('slow');
+    }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+})(jQuery);
+
